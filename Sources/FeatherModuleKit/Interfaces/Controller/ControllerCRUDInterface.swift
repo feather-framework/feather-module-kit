478faed6b1811891b58f4fa091e8eb1787d1588c
László Kovács <atomantinator@gmail.com>
2024-04-22 12:34:37 +0200

    added controller crud

---
+++ Sources/FeatherModuleKit/Interfaces/Controller/ControllerCRUDInterface.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//
+//
+//  Created by mzperx on 19/04/2024.
+//
+
+import FeatherComponent
+import FeatherDatabase
+
+public protocol ControllerModelInterface
+{
+    associatedtype Query: DatabaseQueryInterface
+    associatedtype Patch: PatchInterface
+    associatedtype Update: UpdateInterface
+    associatedtype Create: CreateInterface
+    associatedtype Detail: DetailInterface
+    associatedtype Reference: ReferenceInterface
+    associatedtype List: ListInterface
+}
+
+public protocol ControllerCRUDInterface :
+    ControllerCreate,
+    ControllerDelete,
+    ControllerGet,
+    ControllerList,
+    ControllerPatch,
+    ControllerReference,
+    ControllerUpdate
+{
+    associatedtype ControllerModel: ControllerModelInterface
+    associatedtype Query = ControllerModel.Query
+    associatedtype Patch = ControllerModel.Patch
+    associatedtype Update = ControllerModel.Update
+    associatedtype Create = ControllerModel.Create
+    associatedtype Detail = ControllerModel.Detail
+    associatedtype Reference = ControllerModel.Reference
+    associatedtype List = ControllerModel.List
+}
